d7bea683e4158aa460f76bc8d205ae6fbefa68f5
Nils Lundell <nils.solumfaeste@gmail.com>
2020-03-16 11:32:46 +0100

    added some tiny features and cleaned up the script

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-//window.localStorage.clear();
-
 async function main(){
-    let myStorage = window.localStorage;
-    let key = await fetchKey();
-    if(myStorage.getItem('key') == null){
-        myStorage.setItem('key', key.key);
+    if(window.localStorage.getItem('key') == null){
+        await generateNewKey();
         console.log('New key created.');
     }
-
-    await updateLocalStorage();
-    changeInputMethod();
     
+    await updateLocalStorage(); 
+
+    changeInputMethod(); //loads input fields
+    
+    let searchButton = document.querySelector('#searchButton');
     let searchBar = document.getElementById('searchBar');
     let selectList = document.getElementById('dropDown');
+    let newKeyButton = document.querySelector('#newKey')
     
+    searchButton.addEventListener('click', readItemsAsync);
     searchBar.addEventListener('input', filterResultsAsync);
     selectList.addEventListener('change', changeInputMethod);
+    newKeyButton.addEventListener('click', generateNewKey);
 }
 main();
 const baseUrl = 'https://www.forverkliga.se/JavaScript/api/crud.php?key=' + window.localStorage.getItem('key');
 
+
+//FRONT-END FUNCTIONS
 async function changeInputMethod(){
     let dropDown = document.querySelector('#dropDown');
     let inputFields = document.querySelector('#inputFields');
@@ -43,7 +46,7 @@ async function changeInputMethod(){
             inputFields.appendChild(authorField);
             inputFields.appendChild(button);
 
-            button.addEventListener('click', insertItemAsync);
+            button.addEventListener('click', createItemAsync);
             break;
         
         case 'update':
@@ -68,20 +71,6 @@ async function changeInputMethod(){
     }
 }
 
-async function updateLocalStorage(){
-    const request = 'https://www.forverkliga.se/JavaScript/api/crud.php?key='
-                    + window.localStorage.getItem('key') + 
-                    '&op=select';
-    for(let i = 0; i < 10; i++){
-        let jsonItem = await fetch(request).then(resp => resp.json()).catch(console.log('An error has occured'));
-        if(jsonItem.status == 'success'){
-            jsonItem = JSON.stringify(jsonItem.data); // deserialize data so it can be stored in localStorage
-            window.localStorage.setItem('data', jsonItem);
-            return;
-        }
-    }
-    console.log('Could not fetch data');
-}
 
 async function filterResultsAsync(){
     if(window.localStorage.getItem('data') == null){
@@ -91,7 +80,7 @@ async function filterResultsAsync(){
     document.querySelector('#resultDiv').innerHTML = '';
 
     let jsonItem = window.localStorage.getItem('data');
-    jsonItem = JSON.parse(jsonItem); // data in localStorage is saved as string and thus needs to be pared back into json
+    jsonItem = JSON.parse(jsonItem); // data in localStorage is saved as string and thus needs to be parsed back into json
     jsonItem = jsonItem.filter(item => item.title.includes(searchRequest) || item.author.includes(searchRequest));
 
     for(let i = 0; i < jsonItem.length; i++){
@@ -113,24 +102,94 @@ async function filterResultsAsync(){
     console.log('Search Successful');
 }
 
+
+
+
+//LOCAL STORAGE AND KEY FUNCTIONS
 async function fetchKey(){
     return fetch('https://www.forverkliga.se/JavaScript/api/crud.php?requestKey')
     .then(resp => resp.json());
 }
 
-async function deleteItemAsync(){
-    let id = document.querySelector('#idField').value;
-    let request = baseUrl + `&op=delete&id=${id}`;
+async function generateNewKey(){
+    let newKey = await fetchKey();
+    window.localStorage.setItem('key', newKey.key);
+    document.querySelector('#newKeyStatus').innerHTML = newKey.key;
 
+    await updateLocalStorage();
+
+    console.log('New key created')
+}
+
+async function updateLocalStorage(){
+    const request = 'https://www.forverkliga.se/JavaScript/api/crud.php?key='
+                    + window.localStorage.getItem('key') + 
+                    '&op=select';
     for(let i = 0; i < 10; i++){
         let jsonItem = await fetch(request).then(resp => resp.json());
         if(jsonItem.status == 'success'){
-            console.log(jsonItem.status);
-            document.querySelector('#status').innerHTML = 'Item Deleted';
+            jsonItem = JSON.stringify(jsonItem.data); // deserialize data so it can be stored in localStorage
+            window.localStorage.setItem('data', jsonItem);
+            return;
+        }
+    }
+    let h1 = document.createElement('H1');
+    h1.innerHTML = 'Something went wrong. Please reload page.';
+    document.querySelector('#body').appendChild(h1);
+    console.log('Could not fetch data');
+}
+
+
+//CRUD OPERATIONS
+async function createItemAsync(){
+    let title = document.querySelector('#titleField').value;
+    let author = document.querySelector('#authorField').value;
+    let request = baseUrl + `&op=insert&title=${title}&author=${author}`;
+
+    for(let i = 0; i < 10; i++){
+        let item = await fetch(request).then(resp => resp.json());
+        if(item.status == 'success'){
+            document.querySelector('#status').textContent = 'Item Inserted';
+            console.log('status: ' + item.status + ' id: ' + item.id);
             await updateLocalStorage();
+            return item;
+        }
+    }
+    
+    console.log('Insert Failed. Try again, daddy <3 xoxo');
+    document.querySelector('#status').textContent = 'Insert Failed';
+}
+
+async function readItemsAsync(){
+    let request = baseUrl + `&op=select`;
+    document.querySelector('#resultDiv').innerHTML = '';
+
+    for(let i = 0; i < 10; i++){
+        let jsonItem = await fetch(request).then(resp => resp.json());
+        if(jsonItem.status == 'success')
+        {
+            for(let i = 0; i < jsonItem.data.length; i++){
+                let ul = document.createElement('UL');
+                document.querySelector('#resultDiv').appendChild(ul);
+
+                let li1 = document.createElement('LI');      
+                let li2 = document.createElement('LI');                
+                let li3 = document.createElement('LI');
+
+                li1.innerHTML = jsonItem.data[i].title;
+                li2.innerHTML = jsonItem.data[i].author;
+                li3.innerHTML = jsonItem.data[i].id;
+
+                ul.appendChild(li1);
+                ul.appendChild(li2);
+                ul.appendChild(li3); 
+            }
+            console.log('Search Successful');
             return jsonItem;
         }
     }
+    document.querySelector('#resultDiv').innerHTML = '<p>Request Failed</p>';
+    console.log(`This is so sad. Can we find item? no.`);
 }
 
 async function updateItemAsync(){
@@ -150,53 +209,17 @@ async function updateItemAsync(){
     }
 }
 
-async function getItemAsync(){
-    let request = baseUrl + `&op=select`;
-    document.querySelector('#resultDiv').innerHTML = '';
+async function deleteItemAsync(){
+    let id = document.querySelector('#idField').value;
+    let request = baseUrl + `&op=delete&id=${id}`;
 
     for(let i = 0; i < 10; i++){
         let jsonItem = await fetch(request).then(resp => resp.json());
-        if(jsonItem.status == 'success')
-        {
-            for(let i = 0; i < jsonItem.data.length; i++){
-                let ul = document.createElement('UL');
-                document.querySelector('#resultDiv').appendChild(ul);
-
-                let li1 = document.createElement('LI');      
-                let li2 = document.createElement('LI');                
-                let li3 = document.createElement('LI');
-
-                li1.innerHTML = jsonItem.data[i].title;
-                li2.innerHTML = jsonItem.data[i].author;            // Create a <p> element
-                li3.innerHTML = jsonItem.data[i].id;
-
-                ul.appendChild(li1);
-                ul.appendChild(li2);                                     // Append <p> to <div> with id="myDIV" 
-                ul.appendChild(li3);                                     // Append <p> to <div> with id="myDIV" 
-            }
-            console.log('Search Successful');
+        if(jsonItem.status == 'success'){
+            console.log(jsonItem.status);
+            document.querySelector('#status').innerHTML = 'Item Deleted';
+            await updateLocalStorage();
             return jsonItem;
         }
     }
-    document.querySelector('#resultDiv').textContent = 'Request Failed';
-    console.log(`This is so sad. Can we find item? no.`);
-}
-
-async function insertItemAsync(){
-    let title = document.querySelector('#titleField').value;
-    let author = document.querySelector('#authorField').value;
-    let request = baseUrl + `&op=insert&title=${title}&author=${author}`;
-
-    for(let i = 0; i < 10; i++){
-        let item = await fetch(request).then(resp => resp.json());
-        if(item.status == 'success'){
-            document.querySelector('#status').textContent = 'Item Inserted';
-            console.log('status: ' + item.status + ' id: ' + item.id);
-            await updateLocalStorage();
-            return item;
-        }
-    }
-    
-    console.log('Insert Failed. Try again, daddy <3 xoxo');
-    document.querySelector('#status').textContent = 'Insert Failed';
 }
